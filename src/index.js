39b6805c804691dd5c106d90957a9062ea5349cb
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:09:49 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const { lightningChart, PointShape, Themes } = lcjs
 const pointSize = 10
 
 // Create a XY Chart.
-const chart = lightningChart()
+const chart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
